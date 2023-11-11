a8229778435e9bf99cb0db4792d87850563e8d62
Mu Ye <emailyemu@gmail.com>
2023-11-10 23:06:47 -0800

    Set up socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const socket = io('http://localhost:8000');
+  }, []);
 
   return (
     <>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,34 @@
 import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+
 import dotenv from 'dotenv';
-import indexRouter from './routes/api';
-
-
 dotenv.config();
 
-const app: Express = express();
+import http from 'http';
+const server = http.createServer(app);
+
+import { Server } from 'socket.io';
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+import indexRouter from './routes/api';
+
 const port = process.env.PORT;
 
 // Use the routes defined in routes/index.js
 app.use('/', indexRouter);
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
