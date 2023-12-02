c2005e2df76a9b361808f15b0efdce9736573344
Mu Ye <emailyemu@gmail.com>
2023-12-02 12:09:12 -0800

    Clean up

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,9 +1,4 @@
-import { Request, Response } from 'express';
-
-import Room from '../models/room';
 import User, { IUser } from '../models/user';
-
-export const getUsersForRoom = (req: Request, res: Response): void => {};
 
 export const createUser = async (): Promise<IUser> => {
   const user = new User();
@@ -11,7 +6,4 @@ export const createUser = async (): Promise<IUser> => {
   return user;
 };
 
-export const updateUser = (req: Request, res: Response): void => {
-  // Logic for a player to join a room
-  res.status(201).send('Player updated');
-};
+export const updateUser = async (): Promise<IUser> => {};
