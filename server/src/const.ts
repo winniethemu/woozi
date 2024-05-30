03c68962c2876ed27aadbea05ee92ddfe6bfaefd
Mu Ye <emailyemu@gmail.com>
2024-05-29 22:46:39 -0700

    Project reset

--- server/src/const.ts
+++
@@ -1,5 +0,0 @@
-export const CLIENT_BASE_URL = 'http://localhost:3000';
-
-export const corsOptions = {
-  origin: CLIENT_BASE_URL,
-};
--- server/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generate a random number between [min, max)
- */
-export function random(min: number, max: number) {
-  const delta = Math.floor((max - min) * Math.random());
-  return min + delta;
-}
