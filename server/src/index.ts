2d0ceb91fb01eca3fb9b3735314a72159b9a525a
Mu Ye <emailyemu@gmail.com>
2024-06-11 11:49:16 -0700

    server: proper user existence check

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { EntityId, Repository } from 'redis-om';
+import { EntityId, EntityKeyName, Repository } from 'redis-om';
 import { Server as SocketServer } from 'socket.io';
 import { createClient as createRedisClient } from 'redis';
 import { createServer as createHTTPServer } from 'node:http';
@@ -25,7 +25,8 @@ const userRepository = new Repository(userSchema, redis);
 io.use(async (socket, next) => {
   const token = socket.handshake.auth.token || '';
   const user = await userRepository.fetch(token);
-  if (!user[EntityId]) {
+  const userExists = await redis.exists(user[EntityKeyName] as string);
+  if (!userExists) {
     const record = await userRepository.save({ name: 'Anonymous User' });
     socket.emit(MessageType.SET_USER, {
       id: record[EntityId],
