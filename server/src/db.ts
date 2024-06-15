685496410c77c6fac0bcd4e0c1a74c5b23751e27
Mu Ye <emailyemu@gmail.com>
2024-06-15 16:40:13 -0700

    server: incorporate schemas into db file

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,28 @@
 import { createClient } from 'redis';
-import { Repository } from 'redis-om';
+import { Repository, Schema } from 'redis-om';
 
-import { gameSchema, userSchema } from './models.js';
-
+/**
+ * Init DB
+ */
 const redisClient = createClient();
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 await redisClient.connect();
 
+/**
+ * Schemas
+ */
+const userSchema = new Schema('user', {
+  name: { type: 'string' },
+});
+
+const gameSchema = new Schema('game', {
+  code: { type: 'string' },
+  players: { type: 'string[]' },
+});
+
+/**
+ * Repositories
+ */
 const userRepository = new Repository(userSchema, redisClient);
 const gameRepository = new Repository(gameSchema, redisClient);
 
